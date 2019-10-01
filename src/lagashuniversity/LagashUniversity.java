436900a68b0951c1817b5f955b62785592f41b3a
Agustin Miñan <agus21111996@hotmail.com>
2019-10-01 12:25:16 -0300

    Interfaz Grafica (sin implementar metodos)

--- src/lagashuniversity/LagashUniversity.java
+++ src/lagashuniversity/LagashUniversity.java
@@ -7,14 +7,17 @@ package lagashuniversity;
 public class LagashUniversity {
 
     public static void main(String[] args) {
-//PRUEBA
-        Parquimetro parquimetro = new Parquimetro(50);
-        parquimetro.autoDetectado("AAA111");
-        for (int i = 0; i < 121; i++) {
-            parquimetro.avanzarMinuto();
-        }
-        parquimetro.setCentavosPorHora(10);
-        parquimetro.estacionamientoFinalizado();
+        
+        InterfazGrafica gui = new InterfazGrafica();
+        gui.setVisible(true);
+////PRUEBA
+//        Parquimetro parquimetro = new Parquimetro(50);
+//        parquimetro.autoDetectado("AAA111");
+//        for (int i = 0; i < 121; i++) {
+//            parquimetro.avanzarMinuto();
+//        }
+//        parquimetro.setCentavosPorHora(10);
+//        parquimetro.estacionamientoFinalizado();
     }
 
 }
